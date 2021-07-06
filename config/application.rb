6c10808a3695560af06325c90f99bd016e6f8502
maylis de la monneraye <maylisdlm@hotmail.fr>
2021-07-06 14:44:30 +0200

    g

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module Calendar
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config . i18n . available_locales  =  [ :en ,  :de ,  :fr ]
-    config . i18n . default_locale  =  :fr
+    config.i18n.available_locales = [:en , :de , :fr]
+    config.i18n.default_locale = :fr
 
     # Configuration for the application, engines, and railties goes here.
     #
