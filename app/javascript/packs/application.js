b5cf5306b9e977d84056baa8e5a9df8e15f00f95
maylis de la monneraye <maylisdlm@hotmail.fr>
2021-07-08 15:21:15 +0200

    flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import { initFlatpickr } from "../plugins/flatpickr";
+
+initFlatpickr();
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
