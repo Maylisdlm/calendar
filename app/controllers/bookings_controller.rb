bf2aa51a8959beccf580abddd5617ded0ad1cf7f
maylis de la monneraye <maylisdlm@hotmail.fr>
2021-08-04 15:13:58 +0200

    m

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,11 +5,11 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new
+    @booking = Booking.new(booking_params)
     booking_period = (@booking.date_end - @booking.date_beginning).to_i
     @booking.booking_pricing = @nb_user.count.daily_pricing * booking_period
     if @booking.save
-      redirect_to pages_home_path
+      redirect_to # pages_home_path     ou      booking_path(@booking)
     else
       render :new
     end
@@ -18,5 +18,11 @@ class BookingsController < ApplicationController
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:city, :nb_user, :daily_pricing, :date_beginning, :date_end)
   end
 end
