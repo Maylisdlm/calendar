68d002e345dcb0167dccafd2c893a7ca0ad021db
maylis de la monneraye <maylisdlm@hotmail.fr>
2021-08-06 15:02:26 +0200

    clean

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,27 +18,4 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-
-const check_box = document.querySelector("check_box")
-const price = document.getElementById("price")
-
-if (check_box) {
-  check_box.addEventListener("click", (event) => {
-     event.preventDefault();
-
-  })
-}
-//      const start = document.getElementsByClassName("start")[0].value.split("-");
-//     const endy = document.getElementsByClassName("end")[0].value.split("-");
-//     const daily = document.getElementsByClassName("card-trip-pricing")[0].innerText.slice(0, -1);
-
-//     const dateStart = new Date(parseInt(start[0], 10), parseInt(start[1], 10), parseInt(start[2], 10));
-//     console.log(dateStart);
-//     const dateEndy = new Date(parseInt(endy[0], 10), parseInt(endy[1], 10), parseInt(endy[2], 10));
-//     console.log(dateEndy);
-//     console.log(dateEndy - dateStart);
-//     const one_day = 1000 * 60 * 60 * 24;
-//     const days = Math.round((dateEndy - dateStart) / one_day);
-//     price.innerText = `${days * daily}$`;
-//   })
-// }
+// code javascript
